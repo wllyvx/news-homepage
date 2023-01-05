d57a4f45cdaa228ae9a1c52d5ad1887fc3cdd330
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-01-05 12:46:05 +0700

    added and styled latest news comp for mobile view

--- app/components/LatestNews.tsx
+++ app/components/LatestNews.tsx
@@ -1,5 +1,18 @@
+import data from "../../data/data"
+
 export default function LatestNews() {
+
   return (
-    <div>LatestNews</div>
-  )
+    <div className="bg-very-dark-blue py-6 px-5 mt-16">
+      <h1 className="text-3xl text-soft-orange font-bold">New</h1>
+      <ul className="text-white">
+        {data.latestNews.map((news, index) => (
+          <li key={news.id} className={`${index === data.latestNews.length - 1 ? '' : 'border-b-dark-grayish-blue border-b pb-8'} pt-7`}>
+            <h2 className="text-xl font-bold mb-2">{news.title}</h2>
+            <h3 className="text-grayish-blue text-sm tracking-wider leading-[1.7]">{news.subtitle}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- data/data.ts
+++ data/data.ts
@@ -3,6 +3,23 @@ const data = {
       title: 'The Bright Future of Web 3.0?',
       desc: 'We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?'
     },
+    latestNews: [
+        {
+            id: '1',
+            title: 'Hydrogen VS Electric Cars',
+            subtitle: 'Will hydrogen-fueled cars ever catch up to EVs?'
+        },
+        {
+            id: '2',
+            title: 'The Downsides of AI Artistry',
+            subtitle: 'What are the possible adverse effects of on-demand AI image generation?'
+        },
+        {
+            id: '3',
+            title: 'Is VC Funding Drying Up?',
+            subtitle: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.'
+        },
+    ]
 }
 
 export default data;
